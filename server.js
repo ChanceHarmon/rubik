c77f9e8031264a9eab374fe0638480f12392d7be
Chance Harmon <henrikillsit@gmail.com>
2020-10-07 22:52:03 -0700

    fixed the display of the face color in relation to how it would look in 3d, simple css fix for once

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', error => console.error(error))
-app.use(cors());
 const PORT = process.env.PORT;
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', error => console.error(error));
+
+app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: true }))
-
-
-//All the code ....
-
-//Create an array with 6 sides with colors, and make it a global item
 
 app.get('/', getCube);
 app.post('/testing_form', formInput);
@@ -59,20 +55,12 @@ const rotate = matrix => {
   return matrix
 };
 
-
-
-
-
 function formInput(request, response) {
-
-
-
 
   let travelLog = {};
   let faceValue;
-  console.log(request.body)
+
   if (request.body.cube_color === 'white') {
-    console.log('test')
     faceValue = 'face';
     travelLog.table = faceValue;
   }
@@ -85,53 +73,39 @@ function formInput(request, response) {
 
   client.query(sql)
     .then(results => {
-      console.log(results)
       let faceArray = results.rows.map(item => item.positions.split(','));
       rotate(faceArray);
       travelLog.face_array = faceArray;
     }).then(() => {
       client.query(sql2)
         .then(results => {
-          console.log(results.rows[0].positions.split(','))
           travelLog.top = results.rows[0].positions.split(',')
         })
         .then(() => {
           client.query(sql3)
             .then(results => {
-              console.log(results.rows[0].positions.split(','))
               travelLog.left_hand = results.rows[0].positions.split(',')
             })
             .then(() => {
               client.query(sql4)
                 .then(results => {
-                  console.log(results.rows[0].positions.split(','))
                   travelLog.right_hand = results.rows[0].positions.split(',')
                 })
                 .then(() => {
                   client.query(sql5)
                     .then(results => {
-                      console.log(results.rows[0].positions.split(','))
                       travelLog.bottom = results.rows[0].positions.split(',')
                     })
                     .then(() => {
-                      console.log(travelLog)
-                      //We made it, we have an object with everything we need at an incrdibly ineffcient way, now we nee to keep doing things terribly inneficent : )
-
-
-                      //First thing is to update the face as a whole.  I believe this will have to be a loop with update sql statements in them.
                       let num = 1;
                       while (num < 3) {
                         travelLog.face_array.forEach(item => {
-                          console.log(item)
                           let sql = `UPDATE ${travelLog.table} SET positions='${item}' WHERE id=${num};`
-                          console.log(sql)
                           client.query(sql)
                           num++;
                         })
                       }
-
                     }).then(() => {
-                      //now start rotating rows, refer to notebook for order of clockwise values
                       let sql = `UPDATE top SET positions='${travelLog.left_hand}' WHERE id=1;`;
                       client.query(sql)
                         .then(() => {
@@ -144,7 +118,6 @@ function formInput(request, response) {
                                   let sql = `UPDATE left_hand SET positions='${travelLog.bottom}' WHERE id=1;`;
                                   client.query(sql)
                                     .then(() => {
-                                      console.log('made it')
                                       response.status(200).redirect('/');
                                     })
                                 })
@@ -156,21 +129,6 @@ function formInput(request, response) {
         })
     }).catch('error', error => console.error(error))
 }
-
-
-
-//generic prob, i need to make sure that the array from selecting the cube returns before I can reference the values to loop over, 
-//then I need to figure outt how to make it wait for the next call of looping through each db callllll......
-// }).then(() => {
-//   Promise.all(cubeArray).then(result => {
-//     console.log(result)
-//   })
-
-
-
-
-
-
 
 client.connect()
   .then(() => {
