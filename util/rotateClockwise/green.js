6381c9ec329ab6304b5ddd104f2c2423724e73fc
Chance Harmon <henrikillsit@gmail.com>
2021-02-01 15:45:11 -0800

    counter green is working, don\'t forget what file you are working in

--- util/rotateClockwise/green.js
+++ util/rotateClockwise/green.js
@@ -6,9 +6,9 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 client.connect()
 
-const rotate = require('../../util/rotateClockwiseAlgo');
+const rotateCounter = require('../../util/rotateCounterClockwiseAlgo');
 
-module.exports = function rotateGreenClockwise(str) {
+module.exports = function rotateGreenCounterClockwise(str) {
 
   let travelLog = {};
   let faceValue;
@@ -33,7 +33,7 @@ module.exports = function rotateGreenClockwise(str) {
     .then(results => {
       let presort = results.rows.sort((a, b) => a.id - b.id)
       let faceArray = presort.map(item => item.positions.split(','));
-      rotate(faceArray);
+      rotateCounter(faceArray);
       travelLog.face_array = faceArray;
     }).then(() => {
       client.query(sql2)
