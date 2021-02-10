698731ac1dc3cc0282b416386003be41fa47d26b
Chance Harmon <henrikillsit@gmail.com>
2021-02-09 22:40:27 -0800

    fourth random validated and working

--- util/randomCubePresets/presetOne.js
+++ util/randomCubePresets/presetOne.js
@@ -56,7 +56,6 @@ let sqlArray2 = [
   `UPDATE yellow SET row='bottom', positions='yellow:00,blue:21,blue:20', face_id=6 WHERE id=3;`
 ];
 
-//Currently need to work on array 3, the first two are fixed.
 let sqlArray3 = [
   `UPDATE orange SET row='top', positions='white:00,orange:21,orange:20', face_id=1 WHERE id=1;`,
   `UPDATE orange SET row='middle', positions='green:21,orange:11,green:01', face_id=1 WHERE id=2;`,
@@ -77,28 +76,29 @@ let sqlArray3 = [
   `UPDATE yellow SET row='middle', positions='orange:01,yellow:11,green:10', face_id=6 WHERE id=2;`,
   `UPDATE yellow SET row='bottom', positions='blue:20,red:01,red:00', face_id=6 WHERE id=3;`
 ];
-//Test me ^^^^
 
-// let sqlArray4 = [
-//   `UPDATE orange SET row='top', positions='blue:00,red:10,green:22', face_id=1 WHERE id=1;`,
-//   `UPDATE orange SET row='middle', positions='orange:21,orange:11,yellow:12', face_id=1 WHERE id=2;`,
-//   `UPDATE orange SET row='bottom', positions='yellow:02,blue:01,orange:20', face_id=1 WHERE id=3;`,
-//   `UPDATE green SET row='top', positions='white:20,yellow:01,yellow:00', face_id=2 WHERE id=1;`,
-//   `UPDATE green SET row='middle', positions='white:12,green:11,green:01', face_id=2 WHERE id=2;`,
-//   `UPDATE green SET row='bottom', positions='red:20,green:21,white:22', face_id=2 WHERE id=3;`,
-//   `UPDATE white SET row='top', positions='orange:02,green:10,red:00', face_id=3 WHERE id=1;`,
-//   `UPDATE white SET row='middle', positions='yellow:21,white:11,orange:01', face_id=3 WHERE id=2;`,
-//   `UPDATE white SET row='bottom', positions='red:22,blue:10,green:00', face_id=3 WHERE id=3;`,
-//   `UPDATE blue SET row='top', positions='yellow:22,red:21,blue:22', face_id=4 WHERE id=1;`,
-//   `UPDATE blue SET row='middle', positions='orange:10,blue:11,white:01', face_id=4 WHERE id=2;`,
-//   `UPDATE blue SET row='bottom', positions='white:02,white:21,green:00', face_id=4 WHERE id=3;`,
-//   `UPDATE red SET row='top', positions='blue:02,orange:12,blue:20', face_id=5 WHERE id=1;`,
-//   `UPDATE red SET row='middle', positions='yellow:10,red:11,blue:21', face_id=5 WHERE id=2;`,
-//   `UPDATE red SET row='bottom', positions='green:02,red:12,green:20', face_id=5 WHERE id=3;`,
-//   `UPDATE yellow SET row='top', positions='blue:20,white:10,white:00', face_id=6 WHERE id=1;`,
-//   `UPDATE yellow SET row='middle', positions='blue:21,yellow:11,red:01', face_id=6 WHERE id=2;`,
-//   `UPDATE yellow SET row='bottom', positions='yellow:20,green:01,orange:22', face_id=6 WHERE id=3;`
-// ];
+//Next one
+
+let sqlArray4 = [
+  `UPDATE orange SET row='top', positions='red:20,blue:10,white:20', face_id=1 WHERE id=1;`,
+  `UPDATE orange SET row='middle', positions='green:01,orange:11,orange:21', face_id=1 WHERE id=2;`,
+  `UPDATE orange SET row='bottom', positions='white:02,white:01,blue:22', face_id=1 WHERE id=3;`,
+  `UPDATE green SET row='top', positions='green:02,orange:12,orange:22', face_id=2 WHERE id=1;`,
+  `UPDATE green SET row='middle', positions='yellow:21,green:11,green:12', face_id=2 WHERE id=2;`,
+  `UPDATE green SET row='bottom', positions='orange:20,yellow:01,white:00', face_id=2 WHERE id=3;`,
+  `UPDATE white SET row='top', positions='red:00,red:12,green:22', face_id=3 WHERE id=1;`,
+  `UPDATE white SET row='middle', positions='green:10,white:11,green:21', face_id=3 WHERE id=2;`,
+  `UPDATE white SET row='bottom', positions='green:20,white:21,white:22', face_id=3 WHERE id=3;`,
+  `UPDATE blue SET row='top', positions='blue:00,yellow:10,yellow:00', face_id=4 WHERE id=1;`,
+  `UPDATE blue SET row='middle', positions='white:12,blue:11,white:10', face_id=4 WHERE id=2;`,
+  `UPDATE blue SET row='bottom', positions='red:22,blue:12,blue:02', face_id=4 WHERE id=3;`,
+  `UPDATE red SET row='top', positions='yellow:20,red:01,red:02', face_id=5 WHERE id=1;`,
+  `UPDATE red SET row='middle', positions='red:10,red:11,blue:01', face_id=5 WHERE id=2;`,
+  `UPDATE red SET row='bottom', positions='green:00,yellow:12,yellow:02', face_id=5 WHERE id=3;`,
+  `UPDATE yellow SET row='top', positions='orange:02,blue:21,blue:20', face_id=6 WHERE id=1;`,
+  `UPDATE yellow SET row='middle', positions='red:21,yellow:11,orange:10', face_id=6 WHERE id=2;`,
+  `UPDATE yellow SET row='bottom', positions='yellow:22,orange:01,orange:00', face_id=6 WHERE id=3;`
+];
 
 
 // let sqlArray5 = [
@@ -130,15 +130,15 @@ let randomList = [
   {
     previousViewed: false,
     setList: sqlArray2
+  },
+  {
+    previousViewed: false,
+    setList: sqlArray3
+  },
+  {
+    previousViewed: false,
+    setList: sqlArray4
   }//,
-  // {
-  //   previousViewed: false,
-  //   setList: sqlArray3
-  // },
-  // {
-  //   previousViewed: false,
-  //   setList: sqlArray4
-  // },
   // {
   //   previousViewed: false,
   //   setList: sqlArray5
