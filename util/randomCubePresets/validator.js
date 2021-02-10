3880ad182aaf26a165116d079ae9018f84309349
Chance Harmon <henrikillsit@gmail.com>
2021-02-09 23:27:32 -0800

    fifth random validated and working, sending to master

--- util/randomCubePresets/validator.js
+++ util/randomCubePresets/validator.js
@@ -70,24 +70,24 @@ const obj = {
 //Will return false
 
 let sqlArray = [
-  `UPDATE orange SET row='top', positions='red:20,blue:10,white:20', face_id=1 WHERE id=1;`,
-  `UPDATE orange SET row='middle', positions='green:01,orange:11,orange:21', face_id=1 WHERE id=2;`,
-  `UPDATE orange SET row='bottom', positions='white:02,white:01,blue:22', face_id=1 WHERE id=3;`,
-  `UPDATE green SET row='top', positions='green:02,orange:12,orange:22', face_id=2 WHERE id=1;`,
-  `UPDATE green SET row='middle', positions='yellow:21,green:11,green:12', face_id=2 WHERE id=2;`,
-  `UPDATE green SET row='bottom', positions='orange:20,yellow:01,white:00', face_id=2 WHERE id=3;`,
-  `UPDATE white SET row='top', positions='red:00,red:12,green:22', face_id=3 WHERE id=1;`,
-  `UPDATE white SET row='middle', positions='green:10,white:11,green:21', face_id=3 WHERE id=2;`,
-  `UPDATE white SET row='bottom', positions='green:20,white:21,white:22', face_id=3 WHERE id=3;`,
-  `UPDATE blue SET row='top', positions='blue:00,yellow:10,yellow:00', face_id=4 WHERE id=1;`,
-  `UPDATE blue SET row='middle', positions='white:12,blue:11,white:10', face_id=4 WHERE id=2;`,
-  `UPDATE blue SET row='bottom', positions='red:22,blue:12,blue:02', face_id=4 WHERE id=3;`,
-  `UPDATE red SET row='top', positions='yellow:20,red:01,red:02', face_id=5 WHERE id=1;`,
-  `UPDATE red SET row='middle', positions='red:10,red:11,blue:01', face_id=5 WHERE id=2;`,
-  `UPDATE red SET row='bottom', positions='green:00,yellow:12,yellow:02', face_id=5 WHERE id=3;`,
-  `UPDATE yellow SET row='top', positions='orange:02,blue:21,blue:20', face_id=6 WHERE id=1;`,
-  `UPDATE yellow SET row='middle', positions='red:21,yellow:11,orange:10', face_id=6 WHERE id=2;`,
-  `UPDATE yellow SET row='bottom', positions='yellow:22,orange:01,orange:00', face_id=6 WHERE id=3;`
+  `UPDATE orange SET row='top', positions='red:22,white:01,green:20', face_id=1 WHERE id=1;`,
+  `UPDATE orange SET row='middle', positions='orange:10,orange:11,yellow:10', face_id=1 WHERE id=2;`,
+  `UPDATE orange SET row='bottom', positions='orange:00,green:10,blue:22', face_id=1 WHERE id=3;`,
+  `UPDATE green SET row='top', positions='orange:22,red:10,red:00', face_id=2 WHERE id=1;`,
+  `UPDATE green SET row='middle', positions='green:21,green:11,yellow:12', face_id=2 WHERE id=2;`,
+  `UPDATE green SET row='bottom', positions='orange:20,orange:21,blue:00', face_id=2 WHERE id=3;`,
+  `UPDATE white SET row='top', positions='yellow:20,orange:01,yellow:02', face_id=3 WHERE id=1;`,
+  `UPDATE white SET row='middle', positions='green:12,white:11,white:21', face_id=3 WHERE id=2;`,
+  `UPDATE white SET row='bottom', positions='green:02,red:21,orange:02', face_id=3 WHERE id=3;`,
+  `UPDATE blue SET row='top', positions='white:00,red:01,blue:20', face_id=4 WHERE id=1;`,
+  `UPDATE blue SET row='middle', positions='white:10,blue:11,blue:12', face_id=4 WHERE id=2;`,
+  `UPDATE blue SET row='bottom', positions='red:20,white:12,white:02', face_id=4 WHERE id=3;`,
+  `UPDATE red SET row='top', positions='white:20,yellow:01,green:00', face_id=5 WHERE id=1;`,
+  `UPDATE red SET row='middle', positions='blue:21,red:11,green:01', face_id=5 WHERE id=2;`,
+  `UPDATE red SET row='bottom', positions='red:02,blue:10,green:22', face_id=5 WHERE id=3;`,
+  `UPDATE yellow SET row='top', positions='white:22,red:12,yellow:00', face_id=6 WHERE id=1;`,
+  `UPDATE yellow SET row='middle', positions='yellow:21,yellow:11,blue:01', face_id=6 WHERE id=2;`,
+  `UPDATE yellow SET row='bottom', positions='yellow:22,orange:12,blue:02', face_id=6 WHERE id=3;`
 ];
 
 function splitArray(arr) {
@@ -121,7 +121,7 @@ function testValue(callback, arr, obj) {
         // console.log('changing to -1')
         obj[innerArray[j]] = -1;
       }
-      // console.log(obj)
+      //console.log(obj)
     }
   }
   for (let i in obj) {
