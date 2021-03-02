6c1b88f0920a052f60f50282e7efd4f3f946917a
Chance Harmon <henrikillsit@gmail.com>
2021-03-02 01:22:53 -0800

    user stats added

--- user_schema.sql
+++ user_schema.sql
@@ -7,8 +7,7 @@ CREATE TABLE user_table (
   id SERIAL PRIMARY KEY,
   user_handle VARCHAR(255),
   user_email VARCHAR(255),
-  date_created DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP
+  date_created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 --INSERT INTO user_table(date_created) VALUES('This is the first message.');
@@ -17,9 +16,23 @@ CREATE TABLE user_table (
 
 CREATE TABLE user_stats(
   id SERIAL PRIMAY KEY,
-
-
+  total_game INT,
+  total_wins INT,
+  total_losses INT,
+  total_resets INT,
+  win_vs_loss_record FLOAT,
+  current_game_cube TEXT,
+  current_game_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  current_move_count INT,
+  best_time TIME,
+  best_move_count INT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user_table (id)
 );
+
+
+-- Intitate the Join of FK on the tables
+SELECT * FROM user_stats JOIN user_table ON user_stats.user_id = user_table.id;
 
 
 
